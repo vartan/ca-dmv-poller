3d9e38feb0ab9bccdc2bb1e3af5e3ba39ffb641f
Haoyu Wang <hywang@hywang.mtv.corp.google.com>
2016-10-04 22:53:18 -0700

    Add logic to handle the dynamic js redirect page.

--- poll.js
+++ poll.js
@@ -1,57 +1,84 @@
-"use strict";
-var Q = require("q");
+'use strict';
+var Q = require('q');
 var querystring = require('querystring');
-var sprintf = require("sprintf-js").sprintf;
-var text = require("mtextbelt");
+var sprintf = require('sprintf-js').sprintf;
+var text = require('mtextbelt');
 var gm = require('googlemaps');
-var https = require("https");
-var cheerio = require("cheerio");
-var geolib = require("geolib");
+var https = require('https');
+var cheerio = require('cheerio');
+var geolib = require('geolib');
+var jsdom = require('jsdom');
+var fs = require('fs');
 
 
+var settings = require('./config.json');
+var dmvInfo = require('./DMV_Info.json');
+var HEADERS = {
+  'User-Agent':
+      'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.13) Gecko/20080311 Firefox/2.0.0.13',
 
-var settings = require("./config.json");
-var dmvInfo = require("./DMV_Info.json");
+  'Content-Type': 'application/x-www-form-urlencoded',
+  'Referer': 'https://www.dmv.ca.gov' + getPath(settings)
+};
+
 var found = {};
-console.log("Checking every "+settings.checkEveryMinutes+" minutes, at DMV offices "+settings.maxDistanceMiles+" miles from "+settings.home);
-if(settings.textOnFind) {
-  console.log("Will text "+settings.textNumber+" when a match is found.");
+console.log(
+    'Checking every ' + settings.checkEveryMinutes +
+    ' minutes, at DMV offices ' + settings.maxDistanceMiles + ' miles from ' +
+    settings.home);
+if (settings.textOnFind) {
+  console.log('Will text ' + settings.textNumber + ' when a match is found.');
 }
-
-
 
 
 // Main programming flow
 Q.fcall(getHomeLocation(settings.home))
-  .then(getNearbyDMV(dmvInfo, settings.maxDistanceMiles))
-  .then(checkLoop(settings))
-  .catch(function(e){console.log(e);});
+    .then(getNearbyDMV(dmvInfo, settings.maxDistanceMiles))
+    .then(checkLoop(settings))
+    .catch(function(e) {
+      console.log(e);
+    });
 
 
+/**
+ * @param  {Object} settings User Settings object.
+ * @return {string} The request url path.
+ */
+function getPath(settings) {
+  if (settings.behindTheWheelTest) {
+    return '/wasapp/foa/findDriveTest.do';
+  } else {
+    return '/wasapp/foa/findOfficeVisit.do';
+  }
+}
 
 
 /**
  * Check Loop
  * @param  {settings} settings User Settings
- * @return {None}          Never actually returns
  */
 function checkLoop(settings) {
   return function(dmvInfo) {
     var promise = Q.resolve();
-    for(var i in dmvInfo) {
-        promise = promise
-          .then(makeDMVRequest(dmvInfo[i], settings))
-          .then(checkAppointmentResult(dmvInfo[i].name, settings.dayOfWeeks))
-          .delay(1000*settings.secondsBetweenRequests);
-
+    for (var i in dmvInfo) {
+      promise = promise.then(makeDMVRequest(dmvInfo[i], settings))
+                    .then(handleDMVRedirect())
+                    .then(checkAppointmentResult(
+                        dmvInfo[i].name, settings.dayOfWeeks))
+                    .delay(1000 * settings.secondsBetweenRequests);
     }
-    return promise.then(function() {
-      return Q.resolve(dmvInfo);
-    }).delay(settings.checkEveryMinutes*1000*60).fail(function(e){
-            console.log("Error: "+JSON.stringify(e));
-          }).then(checkLoop(settings));
+    return promise
+        .then(function() {
+          return Q.resolve(dmvInfo);
+        })
+        .delay(settings.checkEveryMinutes * 1000 * 60)
+        .fail(function(e) {
+          console.log('Error: ' + JSON.stringify(e));
+        })
+        .then(checkLoop(settings));
   };
 }
+
 
 /**
  * Make DMV Request
@@ -66,36 +93,24 @@ function makeDMVRequest(dmvInfo, settings) {
 
     var deferred = Q.defer();
     try {
-      var url="www.dmv.ca.gov";
-      var path;
+      var host = 'www.dmv.ca.gov';
+      var path = getPath(settings);
       var post_data = settings.appointmentInfo;
       post_data.officeId = dmvInfo.id;
       post_data.numberItems = 1;
 
-      if(settings.behindTheWheelTest) {
-        path = "/wasapp/foa/findDriveTest.do";
-        post_data.requestedTask="DT";
+      if (settings.behindTheWheelTest) {
+        post_data.requestedTask = 'DT';
       } else {
-        path = "/wasapp/foa/findOfficeVisit.do";
         post_data.taskRWT = true;
       }
 
-
       var postString = querystring.stringify(post_data);
-      var headers = {
-        'User-Agent': 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.13) Gecko/20080311 Firefox/2.0.0.13',
+      var headers = HEADERS;
+      headers['Content-Length'] = postString.length;
 
-        'Content-Type': 'application/x-www-form-urlencoded',
-        'Content-Length': postString.length
-      };
-
-      var options = {
-        host: url,
-        port: 443,
-        path: path,
-        method: 'POST',
-        headers: headers
-      };
+      var options =
+          {host: host, port: 443, path: path, method: 'POST', headers: headers};
 
       // Setup the request.  The options parameter is
       // the object we defined above.
@@ -107,7 +122,6 @@ function makeDMVRequest(dmvInfo, settings) {
         res.on('data', function(data) {
           responseString += data;
         });
-
         res.on('end', function() {
           deferred.resolve(responseString);
         });
@@ -115,26 +129,15 @@ function makeDMVRequest(dmvInfo, settings) {
           deferred.reject(e);
         });
       });
-
-
-
       req.write(postString);
       req.end();
-    } catch(e) {
+    } catch (e) {
       deferred.reject(e);
     }
     return deferred.promise;
 
   };
 }
-
-
-
-
-
-
-
-
 
 
 /**
@@ -146,18 +149,21 @@ function makeDMVRequest(dmvInfo, settings) {
  */
 function getHomeLocation(home) {
   return function() {
-  var deferred = Q.defer();
-    gm.geocode(home, function(err, data){
-      if(!err && data.hasOwnProperty("results") && data.results.hasOwnProperty(0) && data.results[0].hasOwnProperty("geometry")) {
+    var deferred = Q.defer();
+    gm.geocode(home, function(err, data) {
+      if (!err && data.hasOwnProperty('results') &&
+          data.results.hasOwnProperty(0) &&
+          data.results[0].hasOwnProperty('geometry')) {
         var coords = data.results[0].geometry.location;
         deferred.resolve(coords);
       } else {
-        deferred.reject("Could not find your home location.");
+        deferred.reject('Could not find your home location.');
       }
     });
     return deferred.promise;
   };
 }
+
 
 /**
  * Get Nearby DMVs
@@ -173,13 +179,12 @@ function getNearbyDMV(dmvInfo, maxDistanceMiles) {
    */
   return function(homeLocation) {
     var validDMVLocations = [];
-    for(var dmvName in dmvInfo) {
+    for (var dmvName in dmvInfo) {
       var distance = geolib.getDistance(
-          {latitude: homeLocation.lat, longitude:homeLocation.lng},
-          {latitude: dmvInfo[dmvName].lat, longitude: dmvInfo[dmvName].lng}
-      );
-      var distanceMiles = 0.000621371*distance;
-      if(distanceMiles <= maxDistanceMiles) {
+          {latitude: homeLocation.lat, longitude: homeLocation.lng},
+          {latitude: dmvInfo[dmvName].lat, longitude: dmvInfo[dmvName].lng});
+      var distanceMiles = 0.000621371 * distance;
+      if (distanceMiles <= maxDistanceMiles) {
         var obj = dmvInfo[dmvName];
         obj.name = dmvName;
         obj.distanceMiles = distanceMiles;
@@ -187,9 +192,80 @@ function getNearbyDMV(dmvInfo, maxDistanceMiles) {
       }
     }
     return validDMVLocations;
-
   };
 }
+
+
+/**
+ * Handles the DMV js redirect page.
+ * @return {function}        returns a function for Q to call
+ */
+function handleDMVRedirect() {
+  /**
+   * @param  {String} str HTML results of the page request
+   */
+  return function(str) {
+    var deferred = Q.defer();
+    try {
+      jsdom.env({
+        html: str,
+        script: 'challenge();',
+        features: {
+          FetchExternalResources: ['script'],
+          ProcessExternalResources: ['script']
+        },
+        done: function(err, window) {
+          var elements = window.document.forms[0].elements;
+          var secondRequest = {};
+          for (var i = 0; i < elements.length; i++) {
+            var key = elements[i].name;
+            var value = elements[i].value;
+            secondRequest[key] = value;
+          }
+          var host = 'www.dmv.ca.gov';
+          var path = getPath(settings);
+
+          var postString = querystring.stringify(secondRequest);
+          var headers = HEADERS;
+          headers['Content-Length'] = postString.length;
+
+          var options = {
+            host: host,
+            port: 443,
+            path: path,
+            method: 'POST',
+            headers: headers
+          };
+
+          // Setup the request.  The options parameter is
+          // the object we defined above.
+
+          var req = https.request(options, function(res) {
+            res.setEncoding('utf-8');
+            var responseString = '';
+
+            res.on('data', function(data) {
+              responseString += data;
+            });
+            res.on('end', function() {
+              deferred.resolve(responseString);
+            });
+            req.on('error', function(e) {
+              deferred.reject(e);
+            });
+          });
+          req.write(postString);
+          req.end();
+        }
+      });
+    } catch (e) {
+      deferred.reject(e);
+    }
+    return deferred.promise;
+  };
+}
+
+
 /**
  * Check appointment results
  * @param  {String} name     DMV Name
@@ -203,49 +279,56 @@ function checkAppointmentResult(name, schedule) {
   return function(str) {
     var $ = cheerio.load(str);
 
-    var dateString = $("#ApptForm").parent().parent().parent().find("tr:nth-child(3) .alert").text().replace(" at "," ");
-     console.log(name+":\t"+dateString);
-     if(!dateString){
+    var dateString = $('#ApptForm')
+                         .parent()
+                         .parent()
+                         .parent()
+                         .find('tr:nth-child(3) .alert')
+                         .text()
+                         .replace(' at ', ' ');
+    console.log(name + ':\t' + dateString);
+    if (!dateString) {
       displayErrors($);
       return;
-     }
+    }
     var date = new Date(Date.parse(dateString));
     var timeDiff = (date - (new Date()));
     // verify saturday
 
     // only on a saturday
-    var daysUntil = timeDiff/1000/60/60/24;
-    for(var day in schedule) {
+    var daysUntil = timeDiff / 1000 / 60 / 60 / 24;
+    for (var day in schedule) {
       // why is triple equals not working?
       var isDayOfWeek = parseInt(day) === parseInt(date.getDay());
-      //console.log(schedule[day].allowed)
+      // console.log(schedule[day].allowed)
       var withinTime = date.getHours() >= schedule[day].startHour &&
-                        date.getHours() < schedule[day].endHour;
+          date.getHours() < schedule[day].endHour;
       var withinDays = daysUntil < settings.findAppointmentWithinDays;
-      //console.log("within "+settings.findAppointmentWithinDays+" days: "+withinDays);
-      if(withinDays && isDayOfWeek && withinTime && schedule[day].allowed) {
+      // console.log("within "+settings.findAppointmentWithinDays+" days:
+      // "+withinDays);
+      if (withinDays && isDayOfWeek && withinTime && schedule[day].allowed) {
+        if (!found[dateString + name]) {
+          found[dateString + name] = true;
+          console.log('FOUND NEW MATCH! \x07 \n');
 
-        if(!found[dateString+name]) {
-          found[dateString+name] = true;
-          console.log("FOUND NEW MATCH! \x07 \n");
-
-          if(settings.textOnFind) {
-            text.send(settings.textNumber, sprintf("%20s", name)+": "+formatDate(date), function(){});
+          if (settings.textOnFind) {
+            text.send(
+                settings.textNumber,
+                sprintf('%20s', name) + ': ' + formatDate(date), function() {});
           }
 
         } else {
-          console.log("found duplicate match!");
+          console.log('found duplicate match!');
         }
       }
-
     }
   };
 }
 
 
-function displayErrors($){
-  console.log('Match failed - check your config.json. Possible reasons: ')
-  $('.validation_error').each(function(i, element){
+function displayErrors($) {
+  console.log('Match failed - check your config.json. Possible reasons: ');
+  $('.validation_error').each(function(i, element) {
     console.log(element.firstChild.data);
   })
 }
@@ -261,7 +344,10 @@ function formatDate(date) {
   var minutes = date.getMinutes();
   var ampm = hours >= 12 ? 'pm' : 'am';
   hours = hours % 12;
-  hours = hours ? hours : 12; // the hour '0' should be '12'
-  var strTime = sprintf("%02d",hours) + ':' + sprintf("%02d",minutes) + ' ' + ampm;
-  return sprintf("%02d",date.getMonth()+1) + "/" + sprintf("%02d",date.getDate()) + "/" + date.getFullYear() + " " + strTime;
+  hours = hours ? hours : 12;  // the hour '0' should be '12'
+  var strTime =
+      sprintf('%02d', hours) + ':' + sprintf('%02d', minutes) + ' ' + ampm;
+  return sprintf('%02d', date.getMonth() + 1) + '/' +
+      sprintf('%02d', date.getDate()) + '/' + date.getFullYear() + ' ' +
+      strTime;
 }
